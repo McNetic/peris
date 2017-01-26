a4178c3c90ceb85085f8ce01c5cb0b978a82f9f9
Nicolai Ehemann <en@enlightened.de>
2017-01-27 00:25:59 +0100

    Fixed style (according to checkstyle) of BackStackManager

--- app/src/main/java/de/enlightened/peris/BackStackManager.java
+++ app/src/main/java/de/enlightened/peris/BackStackManager.java
@@ -7,97 +7,97 @@ import java.util.ArrayList;
 
 public class BackStackManager {
 
-    private ArrayList<ArrayList<BackStackItem>> backstackArray;
+  private ArrayList<ArrayList<BackStackItem>> backstackArray;
 
-    public BackStackManager() {
-        clearAllStacks();
+  public BackStackManager() {
+    this.clearAllStacks();
+  }
+
+  public final void clearAllStacks() {
+    this.backstackArray = new ArrayList<ArrayList<BackStackItem>>();
+  }
+
+  public final int createBackstack() {
+    final ArrayList<BackStackItem> freshBackstack = new ArrayList<BackStackItem>();
+    this.backstackArray.add(freshBackstack);
+    return this.backstackArray.size() - 1;
+  }
+
+  public final int getBackStackSize(final int backstackId) {
+    return this.backstackArray.get(backstackId).size();
+  }
+
+  public final void addToBackstack(final int backstackId, final int type, final Bundle bundle) {
+    Log.i("Peris", "Backstack Adding Item " + type);
+    final BackStackItem item = new BackStackItem(type, bundle);
+    this.backstackArray.get(backstackId).add(item);
+  }
+
+  public final BackStackItem getActiveItem(final int backstackId) {
+    if (this.backstackArray.get(backstackId).size() == 0) {
+      return null;
     }
 
-    public void clearAllStacks() {
-        backstackArray = new ArrayList<ArrayList<BackStackItem>>();
+    return this.backstackArray.get(backstackId).get(this.backstackArray.get(backstackId).size() - 1);
+  }
+
+  public final BackStackItem getActiveItemAndRemove(final int backstackId) {
+    if (this.backstackArray.get(backstackId).size() == 0) {
+      return null;
     }
 
-    public int createBackstack() {
-        ArrayList<BackStackItem> freshBackstack = new ArrayList<BackStackItem>();
-        backstackArray.add(freshBackstack);
-        return backstackArray.size() - 1;
+    final BackStackItem returnItem = this.backstackArray.get(backstackId).get(this.backstackArray.get(backstackId).size() - 1);
+    this.backstackArray.get(backstackId).remove(this.backstackArray.get(backstackId).size() - 1);
+
+    return returnItem;
+  }
+
+  public final BackStackItem navigateToBase(final int backstackId) {
+    if (this.backstackArray.get(backstackId).size() == 0) {
+      return null;
     }
 
-    public int getBackStackSize(int backstackId) {
-        return backstackArray.get(backstackId).size();
+    final BackStackItem returnItem = this.backstackArray.get(backstackId).get(0);
+    this.backstackArray.get(backstackId).clear();
+    return returnItem;
+  }
+
+  public final BackStackItem navigateBack(final int backstackId) {
+
+    if (this.backstackArray.get(backstackId).size() == 0) {
+      return null;
     }
 
-    public void addToBackstack(int backstackId, int type, Bundle bundle) {
-        Log.i("Peris", "Backstack Adding Item " + type);
-        BackStackItem item = new BackStackItem(type, bundle);
-        backstackArray.get(backstackId).add(item);
+    this.backstackArray.get(backstackId).remove(this.backstackArray.get(backstackId).size() - 1);
+
+    final BackStackItem returnItem = this.backstackArray.get(backstackId).get(this.backstackArray.get(backstackId).size() - 1);
+
+    this.backstackArray.get(backstackId).remove(this.backstackArray.get(backstackId).size() - 1);
+
+    return returnItem;
+  }
+
+  public class BackStackItem {
+
+    public static final int BACKSTACK_TYPE_FORUM = 1;
+    public static final int BACKSTACK_TYPE_TOPIC = 2;
+    public static final int BACKSTACK_TYPE_PROFILE = 3;
+    public static final int BACKSTACK_TYPE_SETTINGS = 4;
+
+    private int type;
+    private Bundle params;
+
+    public BackStackItem(final int itemType, final Bundle itemBundle) {
+      this.type = itemType;
+      this.params = itemBundle;
     }
 
-    public BackStackItem getActiveItem(int backstackId) {
-        if (backstackArray.get(backstackId).size() == 0) {
-            return null;
-        }
-
-        return backstackArray.get(backstackId).get(backstackArray.get(backstackId).size() - 1);
+    public final int getType() {
+      return this.type;
     }
 
-    public BackStackItem getActiveItemAndRemove(int backstackId) {
-        if (backstackArray.get(backstackId).size() == 0) {
-            return null;
-        }
-
-        BackStackItem returnItem = backstackArray.get(backstackId).get(backstackArray.get(backstackId).size() - 1);
-        backstackArray.get(backstackId).remove(backstackArray.get(backstackId).size() - 1);
-
-        return returnItem;
+    public final Bundle getBundle() {
+      return this.params;
     }
-
-    public BackStackItem navigateToBase(int backstackId) {
-        if (backstackArray.get(backstackId).size() == 0) {
-            return null;
-        }
-
-        BackStackItem returnItem = backstackArray.get(backstackId).get(0);
-        backstackArray.get(backstackId).clear();
-        return returnItem;
-    }
-
-    public BackStackItem navigateBack(int backstackId) {
-
-        if (backstackArray.get(backstackId).size() == 0) {
-            return null;
-        }
-
-        backstackArray.get(backstackId).remove(backstackArray.get(backstackId).size() - 1);
-
-        BackStackItem returnItem = backstackArray.get(backstackId).get(backstackArray.get(backstackId).size() - 1);
-
-        backstackArray.get(backstackId).remove(backstackArray.get(backstackId).size() - 1);
-
-        return returnItem;
-    }
-
-    public class BackStackItem {
-
-        public static final int BACKSTACK_TYPE_FORUM = 1;
-        public static final int BACKSTACK_TYPE_TOPIC = 2;
-        public static final int BACKSTACK_TYPE_PROFILE = 3;
-        public static final int BACKSTACK_TYPE_SETTINGS = 4;
-
-        private int type;
-        private Bundle params;
-
-        public BackStackItem(int itemType, Bundle itemBundle) {
-            type = itemType;
-            params = itemBundle;
-        }
-
-        public int getType() {
-            return type;
-        }
-
-        public Bundle getBundle() {
-            return params;
-        }
-    }
+  }
 }
