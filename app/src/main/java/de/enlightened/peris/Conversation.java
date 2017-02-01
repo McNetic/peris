1eac9c336bced26a7325dfdb7fed1840a09a969a
Nicolai Ehemann <en@enlightened.de>
2017-02-01 17:33:47 +0100

    Fixed style (according to checkstyle) of Conversation

--- app/src/main/java/de/enlightened/peris/Conversation.java
+++ app/src/main/java/de/enlightened/peris/Conversation.java
@@ -24,144 +24,123 @@ import java.util.Vector;
 
 public class Conversation extends FragmentActivity {
 
+  private static final int MAX_ITEM_COUNT = 50;
   private String partner;
-  private String partner_name;
+  private String partnerName;
   private ListView conversationList;
-
   private String boxId = "0";
-
   private String senderName = "";
-
   private String conversationModerator;
-
   private SQLiteDatabase notetasticDB;
   private String sql;
-
   private String accent = "";
   private PerisApp application;
-
   private String externalServer = "0";
-
   private Session mailSession;
-
   private AnalyticsHelper ah;
 
   /**
    * Called when the activity is first created.
    */
-
-
   @SuppressLint("NewApi")
   @Override
-  public void onCreate(Bundle savedInstanceState) {
+  public void onCreate(final Bundle savedInstanceState) {
 
-    application = (PerisApp) getApplication();
+    this.application = (PerisApp) getApplication();
 
-    Bundle bundle = getIntent().getExtras();
-    partner = bundle.getString("id");
-    partner_name = bundle.getString("name");
-    conversationModerator = bundle.getString("moderator");
+    final Bundle bundle = getIntent().getExtras();
+    this.partner = bundle.getString("id");
+    this.partnerName = bundle.getString("name");
+    this.conversationModerator = bundle.getString("moderator");
 
     if (bundle.getString("background") != null) {
-      String bgc = bundle.getString("background");
+      final String bgc = bundle.getString("background");
 
       if (bgc.contains("#")) {
-        accent = bgc;
+        this.accent = bgc;
 
       } else {
-        accent = application.getSession().getServer().serverColor;
+        this.accent = this.application.getSession().getServer().serverColor;
       }
     } else {
-      accent = application.getSession().getServer().serverColor;
+      this.accent = this.application.getSession().getServer().serverColor;
     }
 
-    ThemeSetter.setTheme(this, accent);
-
+    ThemeSetter.setTheme(this, this.accent);
     super.onCreate(savedInstanceState);
-
-    ThemeSetter.setActionBar(this, accent);
+    ThemeSetter.setActionBar(this, this.accent);
 
     //Track app analytics
-    ah = ((PerisApp) getApplication()).getAnalyticsHelper();
-    ah.trackScreen(getClass().getName(), false);
+    this.ah = ((PerisApp) getApplication()).getAnalyticsHelper();
+    this.ah.trackScreen(getClass().getName(), false);
 
     setContentView(R.layout.conversation);
 
     if (bundle.getString("boxid") != null) {
-      boxId = bundle.getString("boxid");
+      this.boxId = bundle.getString("boxid");
     }
 
-    setTitle(partner_name);
+    setTitle(this.partnerName);
 
+    final FrameLayout container = (FrameLayout) findViewById(R.id.conversation_list_container);
 
-    FrameLayout container = (FrameLayout) findViewById(R.id.conversation_list_container);
+    this.conversationList = new ListView(this);
+    this.conversationList.setDivider(null);
+    this.conversationList.setLayoutParams(
+        new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT,
+        LayoutParams.MATCH_PARENT));
 
-    conversationList = new ListView(this);
-    conversationList.setDivider(null);
-    conversationList.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-
-    container.addView(conversationList);
+    container.addView(this.conversationList);
 
     if (bundle.containsKey("server")) {
-
-      externalServer = bundle.getString("server");
-
+      this.externalServer = bundle.getString("server");
       Log.i("Peris", "Mail bundle contains server!");
-
-      notetasticDB = this.openOrCreateDatabase("peris", 0, null);
-
-      String cleanServer = DatabaseUtils.sqlEscapeString(bundle.getString("server"));
-
-      sql = "select * from accountlist where _id = " + cleanServer + ";";
-
-      Cursor c = notetasticDB.rawQuery(sql, null);
-
+      this.notetasticDB = this.openOrCreateDatabase("peris", 0, null);
+      final String cleanServer = DatabaseUtils.sqlEscapeString(bundle.getString("server"));
+      this.sql = "select * from accountlist where _id = " + cleanServer + ";";
+      final Cursor c = this.notetasticDB.rawQuery(this.sql, null);
 
       if (c == null) {
-        notetasticDB.close();
+        this.notetasticDB.close();
         return;
       }
-
       Server server = null;
 
       while (c.moveToNext()) {
         server = IntroScreen.parseServerData(c);
       }
 
-      notetasticDB.close();
+      this.notetasticDB.close();
 
       if (server == null) {
         Log.i("Peris", "Conversaion Server is null!");
         return;
       }
-
-      mailSession = new Session(this, ((PerisApp) getApplication()));
-
-
-      mailSession.setSessionListener(new Session.SessionListener() {
+      this.mailSession = new Session(this, (PerisApp) getApplication());
+      this.mailSession.setSessionListener(new Session.SessionListener() {
 
         @Override
         public void onSessionConnected() {
-          new load_inbox().execute();
+          new LoadInboxTask().execute();
         }
 
         @Override
-        public void onSessionConnectionFailed(String reason) {
+        public void onSessionConnectionFailed(final String reason) {
           return;
         }
 
       });
-      mailSession.setServer(server);
+      this.mailSession.setServer(server);
 
 
     } else {
-      mailSession = application.getSession();
+      this.mailSession = this.application.getSession();
 
-      new load_inbox().execute();
+      new LoadInboxTask().execute();
     }
 
     if (getString(R.string.server_location).contentEquals("0")) {
-      if (ForegroundColorSetter.getForegroundDark(mailSession.getServer().serverColor)) {
+      if (ForegroundColorSetter.getForegroundDark(this.mailSession.getServer().serverColor)) {
         getActionBar().setIcon(R.drawable.ic_ab_main_black);
       } else {
         getActionBar().setIcon(R.drawable.ic_ab_main_white);
@@ -174,31 +153,27 @@ public class Conversation extends FragmentActivity {
   @Override
   public void onResume() {
     //new fetchParticipants().execute();
-
-
     super.onResume();
   }
 
   @Override
   public void onStart() {
     super.onStart();
-
   }
 
   @Override
   public void onStop() {
     super.onStop();
-
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
+  public boolean onCreateOptionsMenu(final Menu menu) {
+    final MenuInflater inflater = getMenuInflater();
     inflater.inflate(R.menu.conversation_menu, menu);
 
-    if (ForegroundColorSetter.getForegroundDark(accent)) {
-      MenuItem itemReply = menu.findItem(R.id.convo_menu_reply);
-      MenuItem itemDelete = menu.findItem(R.id.convo_menu_delete);
+    if (ForegroundColorSetter.getForegroundDark(this.accent)) {
+      final MenuItem itemReply = menu.findItem(R.id.convo_menu_reply);
+      final MenuItem itemDelete = menu.findItem(R.id.convo_menu_delete);
 
       itemReply.setIcon(R.drawable.ic_action_reply_dark);
       itemDelete.setIcon(R.drawable.ic_action_discard_dark);
@@ -208,54 +183,55 @@ public class Conversation extends FragmentActivity {
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
+  public boolean onOptionsItemSelected(final MenuItem item) {
     // Handle item selection
     switch (item.getItemId()) {
       case R.id.convo_menu_reply:
-        launchComposer();
-        return true;
+        this.launchComposer();
+        break;
       case R.id.convo_menu_delete:
-        deleteMessage();
-        return true;
+        this.deleteMessage();
+        break;
       default:
         return super.onOptionsItemSelected(item);
     }
+    return true;
   }
 
   private void launchComposer() {
-    Intent myIntent = new Intent(Conversation.this, NewPost.class);
+    final Intent myIntent = new Intent(Conversation.this, NewPost.class);
 
-    Bundle bundle = new Bundle();
-    bundle.putString("postid", (String) partner);
-    bundle.putString("parent", (String) conversationModerator);
-    bundle.putString("category", senderName);
+    final Bundle bundle = new Bundle();
+    bundle.putString("postid", (String) this.partner);
+    bundle.putString("parent", (String) this.conversationModerator);
+    bundle.putString("category", this.senderName);
     bundle.putString("subforum_id", (String) "0");
     bundle.putString("original_text", (String) "");
-    bundle.putString("boxTitle", (String) partner_name);
+    bundle.putString("boxTitle", (String) this.partnerName);
     bundle.putString("picture", (String) "0");
-    bundle.putString("color", (String) accent);
-    bundle.putString("subject", (String) partner_name);
+    bundle.putString("color", (String) this.accent);
+    bundle.putString("subject", (String) this.partnerName);
     bundle.putInt("post_type", (Integer) 4);
 
-    if (!externalServer.contentEquals("0")) {
-      bundle.putString("server", externalServer);
+    if (!this.externalServer.contentEquals("0")) {
+      bundle.putString("server", this.externalServer);
     }
 
     myIntent.putExtras(bundle);
 
-    startActivity(myIntent);
+    this.startActivity(myIntent);
   }
 
   @SuppressLint("NewApi")
   private void deleteMessage() {
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
-      new messageDeleter().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+      new DeleteMessageTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     } else {
-      new messageDeleter().execute();
+      new DeleteMessageTask().execute();
     }
   }
 
-  private class load_inbox extends AsyncTask<String, Void, Object[]> {
+  private class LoadInboxTask extends AsyncTask<String, Void, Object[]> {
 
     // can use UI thread here
     protected void onPreExecute() {
@@ -263,19 +239,15 @@ public class Conversation extends FragmentActivity {
     }
 
     // automatically done on worker thread (separate from UI thread)
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @SuppressWarnings({"unchecked", "rawtypes", "checkstyle:requirethis"})
     protected Object[] doInBackground(final String... args) {
 
-      Object[] result = new Object[50];
-
+      final Object[] result = new Object[MAX_ITEM_COUNT];
       try {
-
-
-        Vector paramz = new Vector();
+        final Vector paramz = new Vector();
         paramz.addElement(partner);
         paramz.addElement(boxId);
         paramz.addElement(true);
-
         result[0] = application.getSession().performSynchronousCall("get_message", paramz);
 
       } catch (Exception e) {
@@ -286,26 +258,19 @@ public class Conversation extends FragmentActivity {
     }
 
     // can use UI thread here
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings({"rawtypes", "checkstyle:requirethis"})
     protected void onPostExecute(final Object[] result) {
       if (result == null) {
         return;
       }
 
-
-      ArrayList<Post> postList = new ArrayList<Post>();
-
+      final ArrayList<Post> postList = new ArrayList<Post>();
       try {
-
-
         for (Object o : result) {
-
           if (o != null) {
-            HashMap map = (HashMap) o;
-
-            Date timestamp = (Date) map.get("sent_date");
-
-            Post po = new Post();
+            final HashMap map = (HashMap) o;
+            final Date timestamp = (Date) map.get("sent_date");
+            final Post po = new Post();
             //po.category_id = category_id;
             //po.subforum_id = subforum_id;
             //po.thread_id = thread_id;
@@ -316,11 +281,9 @@ public class Conversation extends FragmentActivity {
             po.post_author_id = conversationModerator;
             po.post_body = new String((byte[]) map.get("text_body"));
             po.post_avatar = (String) map.get("icon_url");
-
             po.post_id = partner;
             po.post_tagline = "tagline";
             po.post_timestamp = timestamp.toString();
-
 
             postList.add(po);
           }
@@ -334,20 +297,16 @@ public class Conversation extends FragmentActivity {
     }
   }
 
-  private class messageDeleter extends AsyncTask<String, Void, Object[]> {
+  private class DeleteMessageTask extends AsyncTask<String, Void, Object[]> {
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @SuppressWarnings({"unchecked", "rawtypes", "checkstyle:requirethis"})
     @Override
-    protected Object[] doInBackground(String... params) {
-
-      Object[] result = new Object[50];
-
+    protected Object[] doInBackground(final String... params) {
+      final Object[] result = new Object[MAX_ITEM_COUNT];
       try {
-
-        Vector paramz = new Vector();
+        final Vector paramz = new Vector();
         paramz.addElement(partner);
         paramz.addElement(boxId);
-
         result[0] = application.getSession().performSynchronousCall("delete_message", paramz);
       } catch (Exception e) {
         Log.w("Peris", e.getMessage());
@@ -358,7 +317,6 @@ public class Conversation extends FragmentActivity {
     }
 
     protected void onPostExecute(final Object[] result) {
-
       finish();
     }
   }
