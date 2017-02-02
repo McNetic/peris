e410943d3c42baa1ac8aff529a01e6fc4982d0a0
Nicolai Ehemann <en@enlightened.de>
2017-02-02 23:57:51 +0100

    Fixed style (according to checkstyle) of ChatAdapter

--- app/src/main/java/de/enlightened/peris/ChatAdapter.java
+++ app/src/main/java/de/enlightened/peris/ChatAdapter.java
@@ -19,152 +19,126 @@ import java.util.ArrayList;
 
 @SuppressLint("NewApi")
 public class ChatAdapter extends BaseAdapter {
-
-  Context c;
+  private Context context;
   private ArrayList<Chat> data;
   private boolean useShading = false;
   private boolean useOpenSans = false;
-  //private int fontSize = 20;
   private boolean currentAvatarSetting = false;
-
   private PerisApp application;
 
-  ChatAdapter(ArrayList<Chat> data, Context c, PerisApp app) {
+  ChatAdapter(final ArrayList<Chat> data, final Context context, final PerisApp app) {
     this.data = data;
-    this.c = c;
-    application = app;
+    this.context = context;
+    this.application = app;
 
-    if (c == null) {
+    if (context == null) {
       return;
     }
 
-    SharedPreferences app_preferences = c.getSharedPreferences("prefs", 0);
-
-
-    useShading = app_preferences.getBoolean("use_shading", false);
-    useOpenSans = app_preferences.getBoolean("use_opensans", false);
+    final SharedPreferences appPreferences = context.getSharedPreferences("prefs", 0);
+    this.useShading = appPreferences.getBoolean("use_shading", false);
+    this.useOpenSans = appPreferences.getBoolean("use_opensans", false);
     //fontSize = app_preferences.getInt("font_size", 16);
-    currentAvatarSetting = app_preferences.getBoolean("show_images", true);
+    this.currentAvatarSetting = appPreferences.getBoolean("show_images", true);
   }
 
   public int getCount() {
-    // TODO Auto-generated method stub
-    return data.size();
+    return this.data.size();
   }
 
-  public Object getItem(int arg0) {
-    // TODO Auto-generated method stub
-    return data.get(arg0);
+  public Object getItem(final int arg0) {
+    return this.data.get(arg0);
   }
 
-  public long getItemId(int arg0) {
-    // TODO Auto-generated method stub
+  public long getItemId(final int arg0) {
     return arg0;
   }
 
-
   @SuppressLint("InflateParams")
-  public View getView(final int arg0, View arg1, ViewGroup arg2) {
-
-    Chat ch = data.get(arg0);
-
+  public View getView(final int arg0, final View arg1, final ViewGroup arg2) {
+    final Chat ch = this.data.get(arg0);
     View v = arg1;
+    final LayoutInflater vi = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-    LayoutInflater vi = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-    if (ch.getDisplayname().trim().contentEquals(application.getSession().getServer().serverUserName.trim())) {
+    if (ch.getDisplayname().trim().contentEquals(this.application.getSession().getServer().serverUserName.trim())) {
       v = vi.inflate(R.layout.chat_post_me, null);
     } else {
       v = vi.inflate(R.layout.chat_post, null);
     }
 
-    TextView author = (TextView) v.findViewById(R.id.chat_post_author);
-    TextView timestamp = (TextView) v.findViewById(R.id.chat_post_timestamp);
-    TextView post = (TextView) v.findViewById(R.id.chat_post_body);
+    final TextView author = (TextView) v.findViewById(R.id.chat_post_author);
+    final TextView timestamp = (TextView) v.findViewById(R.id.chat_post_timestamp);
+    final TextView post = (TextView) v.findViewById(R.id.chat_post_body);
 
-    Typeface opensans = Typeface.createFromAsset(c.getAssets(), "fonts/opensans.ttf");
+    final Typeface opensans = Typeface.createFromAsset(this.context.getAssets(), "fonts/opensans.ttf");
+    final LinearLayout llColorBackground = (LinearLayout) v.findViewById(R.id.chat_background);
 
-    LinearLayout ll_color_background = (LinearLayout) v.findViewById(R.id.chat_background);
-
-    String textColor = c.getString(R.string.default_text_color);
-
-    if (application.getSession().getServer().serverTextColor.contains("#")) {
-      textColor = application.getSession().getServer().serverTextColor;
+    String textColor = this.context.getString(R.string.default_text_color);
+    if (this.application.getSession().getServer().serverTextColor.contains("#")) {
+      textColor = this.application.getSession().getServer().serverTextColor;
     }
-
     textColor = "#000000";
 
-    String boxColor = c.getString(R.string.default_element_background);
+    String boxColor = this.context.getString(R.string.default_element_background);
     //String bgColor = c.getString(R.string.default_background);
-
-    if (application.getSession().getServer().serverBoxColor != null) {
-      boxColor = application.getSession().getServer().serverBoxColor;
+    if (this.application.getSession().getServer().serverBoxColor != null) {
+      boxColor = this.application.getSession().getServer().serverBoxColor;
     }
 
-		/*
+    /*
     if(application.getSession().getServer().serverBackground != null) {
-			bgColor = application.getSession().getServer().serverBackground;
-		}
-		*/
+      bgColor = application.getSession().getServer().serverBackground;
+    }
+    */
 
     if (ch.getDisplaycolor().contains("#")) {
       boxColor = ch.getDisplaycolor().replace("#", "#33");
     }
 
-    ImageView chat_ting = (ImageView) v.findViewById(R.id.chat_ting);
-
+    final ImageView chatTing = (ImageView) v.findViewById(R.id.chat_ting);
     if (boxColor.contains("#")) {
-      ll_color_background.setBackgroundColor(Color.parseColor(boxColor));
-      chat_ting.setColorFilter(Color.parseColor(boxColor.replace("#33", "#")));
+      llColorBackground.setBackgroundColor(Color.parseColor(boxColor));
+      chatTing.setColorFilter(Color.parseColor(boxColor.replace("#33", "#")));
     } else {
-      ll_color_background.setBackground(null);
-      chat_ting.setVisibility(View.GONE);
+      llColorBackground.setBackground(null);
+      chatTing.setVisibility(View.GONE);
     }
 
-
-    if (useOpenSans) {
+    if (this.useOpenSans) {
       author.setTypeface(opensans);
       post.setTypeface(opensans);
       timestamp.setTypeface(opensans);
     }
 
-    if (useShading) {
+    if (this.useShading) {
       author.setShadowLayer(2, 0, 0, Color.parseColor("#66000000"));
       post.setShadowLayer(2, 0, 0, Color.parseColor("#66000000"));
       timestamp.setShadowLayer(2, 0, 0, Color.parseColor("#66000000"));
     }
 
-    ImageView avatar = (ImageView) v.findViewById(R.id.chat_avatar);
-
-    ImageView chat_avatar_frame = (ImageView) v.findViewById(R.id.chat_avatar_frame);
-    chat_avatar_frame.setColorFilter(Color.parseColor("#dddddd"));
-
+    final ImageView avatar = (ImageView) v.findViewById(R.id.chat_avatar);
+    ImageView chatAvatarFrame = (ImageView) v.findViewById(R.id.chat_avatar_frame);
+    chatAvatarFrame.setColorFilter(Color.parseColor("#dddddd"));
     author.setText(ch.getDisplayname());
     timestamp.setText(ch.getTimestamp());
     post.setText(ch.getPostbody());
-
     author.setTextColor(Color.parseColor(textColor));
     timestamp.setTextColor(Color.parseColor(textColor));
     post.setTextColor(Color.parseColor(textColor));
 
 
-    if (currentAvatarSetting) {
+    if (this.currentAvatarSetting) {
       if (ch.getDisplayavatar().contains("http://")) {
-        String imageUrl = ch.getDisplayavatar();
+        final String imageUrl = ch.getDisplayavatar();
         ImageLoader.getInstance().displayImage(imageUrl, avatar);
       } else {
         avatar.setImageResource(R.drawable.no_avatar);
       }
     } else {
       avatar.setVisibility(View.GONE);
-
-      chat_avatar_frame = (ImageView) v.findViewById(R.id.post_avatar_frame);
-      chat_avatar_frame.setVisibility(View.GONE);
+      chatAvatarFrame = (ImageView) v.findViewById(R.id.post_avatar_frame);
+      chatAvatarFrame.setVisibility(View.GONE);
     }
-
-
     return v;
   }
-
-
 }
