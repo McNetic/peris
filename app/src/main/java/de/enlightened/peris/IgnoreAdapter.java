30a0cd8dba7b9f02428c3bfe1c46d761f3412608
Nicolai Ehemann <en@enlightened.de>
2017-02-03 19:25:14 +0100

    Fixed style (according to checkstyle) of IgnoreAdapter

--- app/src/main/java/de/enlightened/peris/IgnoreAdapter.java
+++ app/src/main/java/de/enlightened/peris/IgnoreAdapter.java
@@ -20,67 +20,60 @@ import java.util.ArrayList;
 @SuppressLint("NewApi")
 public class IgnoreAdapter extends BaseAdapter {
 
-  Context c;
+  private Context context;
   private ArrayList<IgnoreItem> data;
   private String theme;
   private ImageLoader imageLoader;
 
   @SuppressWarnings("deprecation")
-  IgnoreAdapter(ArrayList<IgnoreItem> data, Context c) {
+  IgnoreAdapter(final ArrayList<IgnoreItem> data, final Context context) {
     this.data = data;
-    this.c = c;
+    this.context = context;
 
-    SharedPreferences app_preferences = c.getSharedPreferences("prefs", 0);
+    final SharedPreferences appPreferences = context.getSharedPreferences("prefs", 0);
     String serverPrefix = "";
-    String server_address = app_preferences.getString("server_address", c.getString(R.string.server_location));
-    if (c.getString(R.string.server_location).contentEquals("0")) {
-      serverPrefix = server_address + "_";
+    final String serverAddress = appPreferences.getString("server_address", context.getString(R.string.server_location));
+    if (context.getString(R.string.server_location).contentEquals("0")) {
+      serverPrefix = serverAddress + "_";
     }
-    theme = Integer.toString(app_preferences.getInt(serverPrefix + "loggedThemeInt", Integer.parseInt(c.getString(R.string.default_theme))));
+    this.theme = Integer.toString(appPreferences.getInt(serverPrefix + "loggedThemeInt", Integer.parseInt(context.getString(R.string.default_theme))));
 
-    imageLoader = ImageLoader.getInstance();
-    DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().cacheInMemory().cacheOnDisc().build();
-    ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(c).defaultDisplayImageOptions(defaultOptions).build();
+    this.imageLoader = ImageLoader.getInstance();
+    final DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().cacheInMemory().cacheOnDisc().build();
+    final ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context).defaultDisplayImageOptions(defaultOptions).build();
     ImageLoader.getInstance().init(config);
 
     //imageLoader.displayImage(imageUrl, imageView);
   }
 
   public int getCount() {
-    // TODO Auto-generated method stub
-    return data.size();
+    return this.data.size();
   }
 
-  public Object getItem(int arg0) {
-    // TODO Auto-generated method stub
-    return data.get(arg0);
+  public Object getItem(final int arg0) {
+    return this.data.get(arg0);
   }
 
-  public long getItemId(int arg0) {
-    // TODO Auto-generated method stub
+  public long getItemId(final int arg0) {
     return arg0;
   }
 
-  public View getView(final int arg0, View arg1, ViewGroup arg2) {
+  public View getView(final int arg0, final View arg1, final ViewGroup arg2) {
     View v = arg1;
     if (v == null) {
-      LayoutInflater vi = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+      final LayoutInflater vi = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
       v = vi.inflate(R.layout.ignore_item, null);
-
-
     }
 
-    TextView iiUsername = (TextView) v.findViewById(R.id.ignore_item_username);
-    TextView iiTimestamp = (TextView) v.findViewById(R.id.ignore_item_timestamp);
-    ImageView iiAvatar = (ImageView) v.findViewById(R.id.ignore_item_avatar);
-
-    IgnoreItem ii = data.get(arg0);
+    final TextView iiUsername = (TextView) v.findViewById(R.id.ignore_item_username);
+    final TextView iiTimestamp = (TextView) v.findViewById(R.id.ignore_item_timestamp);
+    final ImageView iiAvatar = (ImageView) v.findViewById(R.id.ignore_item_avatar);
+    final IgnoreItem ii = this.data.get(arg0);
 
     iiUsername.setText(ii.ignoreItemUsername);
     iiTimestamp.setText("Ignored on " + ii.ignoreItemDate);
 
-    if (theme.contentEquals("1") || theme.contentEquals("2") || theme.contentEquals("3") || theme.contentEquals("6")) {
+    if (this.theme.contentEquals("1") || this.theme.contentEquals("2") || this.theme.contentEquals("3") || this.theme.contentEquals("6")) {
       iiUsername.setTextColor(Color.parseColor("#ffffff"));
       iiTimestamp.setTextColor(Color.parseColor("#ffffff"));
     } else {
@@ -89,13 +82,11 @@ public class IgnoreAdapter extends BaseAdapter {
     }
 
     if (ii.ignoreItemAvatar.contains("http://")) {
-      String imageUrl = ii.ignoreItemAvatar;
-      imageLoader.displayImage(imageUrl, iiAvatar);
+      final String imageUrl = ii.ignoreItemAvatar;
+      this.imageLoader.displayImage(imageUrl, iiAvatar);
     } else {
       iiAvatar.setImageResource(R.drawable.no_avatar);
     }
-
     return v;
   }
-
 }
