75a84fca95a4e007e29f36a9804d398bf88b6863
Nicolai Ehemann <en@enlightened.de>
2017-02-03 19:29:48 +0100

    Fixed style (according to checkstyle) of BanHammerDialogFragment

--- app/src/main/java/de/enlightened/peris/BanHammerDialogFragment.java
+++ app/src/main/java/de/enlightened/peris/BanHammerDialogFragment.java
@@ -17,32 +17,29 @@ import android.widget.TextView;
 import java.util.Vector;
 
 public class BanHammerDialogFragment extends DialogFragment {
-
   private TextView tvIntro;
   private EditText etReason;
   private Button submitButton;
   private ProgressBar banWorking;
-
   private PerisApp application;
-
   private String banId;
   private String banReason;
-  private OnClickListener submitBan = new OnClickListener() {
+  private OnClickListener submitBanListener = new OnClickListener() {
 
     @Override
-    public void onClick(View v) {
+    @SuppressWarnings("checkstyle:requirethis")
+    public void onClick(final View v) {
       banReason = etReason.getText().toString().trim();
       submitButton.setEnabled(false);
       banWorking.setVisibility(View.VISIBLE);
-      new banSubmitter().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, banId, banReason);
+      new SubmitBanTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, banId, banReason);
     }
 
   };
 
   static BanHammerDialogFragment newInstance() {
-    BanHammerDialogFragment f = new BanHammerDialogFragment();
-
-    Bundle args = new Bundle();
+    final BanHammerDialogFragment f = new BanHammerDialogFragment();
+    final Bundle args = new Bundle();
     args.putString("username", "cylon");
     f.setArguments(args);
 
@@ -50,77 +47,58 @@ public class BanHammerDialogFragment extends DialogFragment {
   }
 
   @Override
-  public void onCreate(Bundle savedInstanceState) {
+  public void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     this.setStyle(STYLE_NO_TITLE, getTheme());
   }
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View v = inflater.inflate(R.layout.ban_submission, container, false);
+  public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+    final View v = inflater.inflate(R.layout.ban_submission, container, false);
+    this.tvIntro = (TextView) v.findViewById(R.id.tvBanIntro);
+    this.etReason = (EditText) v.findViewById(R.id.etBanReason);
+    this.submitButton = (Button) v.findViewById(R.id.btnBanSubmit);
+    this.banWorking = (ProgressBar) v.findViewById(R.id.ban_dialog_working);
+    this.banWorking.setVisibility(View.GONE);
 
-    tvIntro = (TextView) v.findViewById(R.id.tvBanIntro);
-    etReason = (EditText) v.findViewById(R.id.etBanReason);
-    submitButton = (Button) v.findViewById(R.id.btnBanSubmit);
-    banWorking = (ProgressBar) v.findViewById(R.id.ban_dialog_working);
-
-    banWorking.setVisibility(View.GONE);
-
-    Bundle args = getArguments();
-    tvIntro.setText("Ban " + args.getString("username") + " for the following reason:");
-
-    banId = args.getString("username");
-
-    submitButton.setOnClickListener(submitBan);
-
-    application = (PerisApp) getActivity().getApplication();
+    final Bundle args = getArguments();
+    this.tvIntro.setText("Ban " + args.getString("username") + " for the following reason:");
+    this.banId = args.getString("username");
+    this.submitButton.setOnClickListener(this.submitBanListener);
+    this.application = (PerisApp) getActivity().getApplication();
 
     return v;
   }
 
-  private class banSubmitter extends AsyncTask<String, Void, String> {
-
+  private class SubmitBanTask extends AsyncTask<String, Void, String> {
     // parm[0] - (string)topic_id
 
     @SuppressLint("UseValueOf")
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @SuppressWarnings({"unchecked", "rawtypes", "checkstyle:requirethis"})
     @Override
-    protected String doInBackground(String... params) {
-
+    protected String doInBackground(final String... params) {
       if (getActivity() == null) {
         return null;
       }
 
-      String result = "";
-
-
       try {
-
         Vector paramz;
-
         paramz = new Vector();
         paramz.addElement(params[0].getBytes());
         paramz.addElement(1);
         paramz.addElement(params[1].getBytes());
-
         application.getSession().performSynchronousCall("m_ban_user", paramz);
-
       } catch (Exception ex) {
         Log.w("Peris", ex.getMessage());
       }
-
-      return result;
+      return "";
     }
 
     protected void onPostExecute(final String result) {
-
       if (getActivity() == null) {
         return;
       }
-
       BanHammerDialogFragment.this.dismiss();
     }
   }
-
 }
