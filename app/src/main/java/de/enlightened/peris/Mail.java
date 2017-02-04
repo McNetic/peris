70ad1bd281ca7c95c22063545c98c06917c64069
Nicolai Ehemann <en@enlightened.de>
2017-02-04 13:29:11 +0100

    Fixed style (according to checkstyle) of Mail

--- app/src/main/java/de/enlightened/peris/Mail.java
+++ app/src/main/java/de/enlightened/peris/Mail.java
@@ -16,54 +16,40 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 public class Mail extends FragmentActivity {
 
   @SuppressLint("NewApi")
-  public void onCreate(Bundle savedInstanceState) {
-
-    PerisApp application = (PerisApp) getApplication();
-    String background = application.getSession().getServer().serverColor;
-
+  public void onCreate(final Bundle savedInstanceState) {
+    final PerisApp application = (PerisApp) getApplication();
+    final String background = application.getSession().getServer().serverColor;
 
     ThemeSetter.setTheme(this, background);
-
     super.onCreate(savedInstanceState);
-
     ThemeSetter.setActionBar(this, background);
 
     setTitle("Inbox");
-
-
     setContentView(R.layout.single_frame_activity);
 
     //Setup forum background
-    String forumWallpaper = application.getSession().getServer().serverWallpaper;
-    String forumBackground = application.getSession().getServer().serverBackground;
-
-    FrameLayout sfa_holder = (FrameLayout) findViewById(R.id.sfa_holder);
-    ImageView sfa_wallpaper = (ImageView) findViewById(R.id.sfa_wallpaper);
-
+    final String forumWallpaper = application.getSession().getServer().serverWallpaper;
+    final String forumBackground = application.getSession().getServer().serverBackground;
+    final FrameLayout sfaHolder = (FrameLayout) findViewById(R.id.sfa_holder);
+    final ImageView sfaWallpaper = (ImageView) findViewById(R.id.sfa_wallpaper);
     if (forumBackground != null && forumBackground.contains("#") && forumBackground.length() == 7) {
-      sfa_holder.setBackgroundColor(Color.parseColor(forumBackground));
+      sfaHolder.setBackgroundColor(Color.parseColor(forumBackground));
     } else {
-      sfa_holder.setBackgroundColor(Color.parseColor(getString(R.string.default_background)));
+      sfaHolder.setBackgroundColor(Color.parseColor(getString(R.string.default_background)));
     }
 
     if (forumWallpaper != null && forumWallpaper.contains("http")) {
-
-      String imageUrl = forumWallpaper;
-      ImageLoader.getInstance().displayImage(imageUrl, sfa_wallpaper);
+      final String imageUrl = forumWallpaper;
+      ImageLoader.getInstance().displayImage(imageUrl, sfaWallpaper);
     } else {
-      sfa_wallpaper.setVisibility(View.GONE);
+      sfaWallpaper.setVisibility(View.GONE);
     }
 
-    MailFragment mf = new MailFragment();
-
-    FragmentManager fragmentManager = getSupportFragmentManager();
-    FragmentTransaction ftZ = null;
-    ftZ = fragmentManager.beginTransaction();
+    final MailFragment mf = new MailFragment();
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    final FragmentTransaction ftZ = fragmentManager.beginTransaction();
     ftZ.replace(R.id.single_frame_layout_frame, mf);
     ftZ.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
     ftZ.commit();
-
   }
-
-
 }
