599594f020c9751cfd9346f2478943c1cbf66de9
Nicolai Ehemann <en@enlightened.de>
2017-02-04 01:31:23 +0100

    Fixed style (according to checkstyle) of ConversationParticipant

--- app/src/main/java/de/enlightened/peris/ConversationParticipant.java
+++ app/src/main/java/de/enlightened/peris/ConversationParticipant.java
@@ -12,83 +12,82 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 public class ConversationParticipant extends ImageView {
+  private static final float AVATAR_MARGIN = 6F;
+  private static final float AVATAR_SIZE = 48F;
+  private static final int AVATAR_ALPHA = 100;
 
   private ImageLoader imageLoader;
-
   private String username;
   private String userid;
   private String userColor;
   private String userStatus;
 
   @SuppressWarnings("deprecation")
-  public ConversationParticipant(Context context) {
+  public ConversationParticipant(final Context context) {
     super(context);
 
-    imageLoader = ImageLoader.getInstance();
-    DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().cacheInMemory().cacheOnDisc().build();
-    ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context).defaultDisplayImageOptions(defaultOptions).build();
+    this.imageLoader = ImageLoader.getInstance();
+    final DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().cacheInMemory().cacheOnDisc().build();
+    final ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context).defaultDisplayImageOptions(defaultOptions).build();
     ImageLoader.getInstance().init(config);
 
-    Resources r = getResources();
+    final Resources r = getResources();
+    final int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, AVATAR_MARGIN, r.getDisplayMetrics());
+    final int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, AVATAR_SIZE, r.getDisplayMetrics());
 
-    int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 6, r.getDisplayMetrics());
-    int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, r.getDisplayMetrics());
-
-    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(size, size);
+    final LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(size, size);
     lp.setMargins(margin, margin, margin, margin);
-    setLayoutParams(lp);
-
-    setScaleType(ScaleType.CENTER_CROP);
+    this.setLayoutParams(lp);
+    this.setScaleType(ScaleType.CENTER_CROP);
   }
 
-  public void setUserid(String userid) {
+  public void setUserid(final String userid) {
     this.userid = userid;
   }
 
   public String getUsername() {
-    return username;
+    return this.username;
   }
 
-  public void setUsername(String username) {
+  public void setUsername(final String username) {
     this.username = username;
   }
 
   public String getUserId() {
-    return userid;
+    return this.userid;
   }
 
   public String getUserColor() {
-    return userColor;
+    return this.userColor;
   }
 
-  public void setUserColor(String color) {
+  public void setUserColor(final String color) {
     this.userColor = color;
   }
 
   public String getUserStatus() {
-    return userStatus;
+    return this.userStatus;
   }
 
   @SuppressWarnings("deprecation")
   @SuppressLint("NewApi")
-  public void setUserStatus(String status) {
+  public void setUserStatus(final String status) {
     this.userStatus = status;
 
     if (status.contentEquals("O")) {
       if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN) {
-        this.setAlpha(100);
+        this.setAlpha(AVATAR_ALPHA);
       } else {
-        this.setImageAlpha(100);
+        this.setImageAlpha(AVATAR_ALPHA);
       }
     }
   }
 
-  public void setImage(String imageURL) {
+  public void setImage(final String imageURL) {
     if (imageURL.contains("http://")) {
-      imageLoader.displayImage(imageURL, this);
+      this.imageLoader.displayImage(imageURL, this);
     } else {
-      setImageResource(R.drawable.no_avatar);
+      this.setImageResource(R.drawable.no_avatar);
     }
   }
-
 }
